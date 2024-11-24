1319bd29aac6b599567478df67e06cbefad9fb17
Luis <solarluiso@gmail.com>
2024-11-24 11:29:26 -0500

    book schema and database connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const { config } = require("dotenv");
 config();
 
+// using express for the middlewares
 const app = express();
+app.use(bodyParser.json());
+
+// connect the database
+mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
+const db = mongoose.connection;
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
